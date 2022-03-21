ed00b60aefdc650c8f083e4a67f83e8702a985df
NoobDeveloper412 <74310120+NoobDeveloper412@users.noreply.github.com>
2022-03-21 22:42:54 +0500

    Added the intray for the app.

---
+++ productivity_app/lib/models/global.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Color darkGreyColor = new Color(0xFF3A3A3A);
+Color blueColor = new Color(0xFF3CAABF);
+
+TextStyle intrayTitleStyle = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.bold,
+    color: darkGreyColor,
+    fontSize: 48);
